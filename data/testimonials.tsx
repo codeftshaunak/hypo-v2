74526ef97cab6e3a4401a7d817a591059eedd937
codeftshaunak <sagardeyyash19@gmail.com>
2024-06-05 18:25:11 +0600

    hypo intialization

---
+++ data/testimonials.tsx
@@ -0,0 +1,42 @@
+export default {
+  title: 'Loved by tech people',
+  items: [
+    {
+      name: 'Eelco Wiersma',
+      description: 'Founder HypoMatrix',
+      avatar:
+        'https://pbs.twimg.com/profile_images/1470742164024008706/k-eXHtu0_normal.jpg',
+      children: (
+        <>
+          “With HypoMatrix I am able to produce high quality dashboard apps and
+          internal tools without a lot of design skills. The ROI really is
+          amazing.”
+        </>
+      ),
+    },
+    {
+      name: 'Caroline Yahaya',
+      description: 'Fullstack Developer',
+      avatar: '/static/images/avatar2.jpg',
+      children: (
+        <>
+          “HypoMatrix allows me to build beautiful and functional UI super fast.
+          The components are very well thought out and the Next.js starter kit
+          saved me at least 100 hours of work.”
+        </>
+      ),
+    },
+    {
+      name: 'Alberto Vazquez',
+      description: 'Frontend Engineer',
+      avatar: '/static/images/avatar3.jpg',
+      children: (
+        <>
+          “HypoMatrix gave us a perfect starting point for our project, having the
+          storybooks already set up with beautifully crafted components and
+          fully mocked pages was a treat.”
+        </>
+      ),
+    },
+  ],
+}
