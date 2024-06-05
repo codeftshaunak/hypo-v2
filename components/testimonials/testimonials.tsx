a72bf55e2454ca236b8585f36f1ce1b4e30de6eb
tonmoydeb404 <tonmoydeb404@gmail.com>
2024-06-05 20:04:18 +0600

    updated some contents

--- components/testimonials/testimonials.tsx
+++ components/testimonials/testimonials.tsx
@@ -1,26 +1,31 @@
-import { ResponsiveValue, SimpleGrid, Stack } from '@chakra-ui/react'
+import { ResponsiveValue, SimpleGrid } from "@chakra-ui/react";
 import {
   Section,
   SectionProps,
   SectionTitle,
   SectionTitleProps,
-} from 'components/section'
-import { Testimonial } from './testimonial'
+} from "components/section";
 
 export interface TestimonialsProps
-  extends Omit<SectionProps, 'title'>,
-    Pick<SectionTitleProps, 'title' | 'description'> {
-  columns?: ResponsiveValue<number>
+  extends Omit<SectionProps, "title">,
+    Pick<SectionTitleProps, "title" | "description"> {
+  columns?: ResponsiveValue<number>;
 }
 
 export const Testimonials: React.FC<TestimonialsProps> = (props) => {
-  const { children, title, columns = [1, null, 2], ...rest } = props
+  const {
+    children,
+    title,
+    description,
+    columns = [1, null, 2],
+    ...rest
+  } = props;
   return (
     <Section {...rest}>
-      <SectionTitle title={title} />
+      <SectionTitle title={title} description={description} />
       <SimpleGrid columns={columns} spacing="8">
         {children}
       </SimpleGrid>
     </Section>
-  )
-}
+  );
+};
