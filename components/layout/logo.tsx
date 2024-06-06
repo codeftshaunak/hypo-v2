e8b495f8f201346cc8171d206771aa708357479c
tonmoydeb404 <tonmoydeb404@gmail.com>
2024-06-06 13:11:54 +0600

    fixed ui issues

--- components/layout/logo.tsx
+++ components/layout/logo.tsx
@@ -22,7 +22,7 @@ export const Logo = ({ href = "/", onClick }: LogoProps) => {
   }
 
   return (
-    <Flex h="8" flexShrink="0" alignItems="flex-start">
+    <Flex flexShrink="0" alignItems="flex-start">
       <Link href={href} display="flex" borderRadius="sm" onClick={onClick}>
         {logo}
         <VisuallyHidden>{siteConfig.seo?.title}</VisuallyHidden>
