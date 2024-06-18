c92ddaadec1ceeca99f6902672a0f9bde162e1a4
tonmoydeb404 <tonmoydeb404@gmail.com>
2024-06-18 09:28:25 +0600

    updated teams card & work card

--- views/home-view/sections/works-section/work-card.tsx
+++ views/home-view/sections/works-section/work-card.tsx
@@ -1,19 +1,17 @@
 import {
-  Badge,
   Box,
+  Card,
+  CardBody,
   Flex,
   IconButton,
   Link,
-  Stack,
   Text,
-  useBoolean,
   useBreakpointValue,
   useColorModeValue,
 } from "@chakra-ui/react";
 import { MotionBox } from "components/motion/box";
 import { Variants } from "framer-motion";
 import Image from "next/image";
-import { FaBook, FaBookOpen } from "react-icons/fa";
 import { FiExternalLink } from "react-icons/fi";
 import { ProjectType } from "types/work";
 
@@ -24,7 +22,7 @@ type Props = {
 
 // ----------------------------------------------------------------------
 
-const detailsVariants: Variants = {
+const tagsVariants: Variants = {
   show: {
     opacity: 1,
     translateY: 0,
@@ -36,83 +34,112 @@ const detailsVariants: Variants = {
 };
 
 const buttonVariants: Variants = {
-  showButton: {
+  show: {
     translateY: 0,
     opacity: 1,
   },
-  hideButton: {
+  hide: {
     translateY: -100,
     opacity: 0,
+  },
+  inview: {
+    translateY: 0,
+    opacity: 1,
   },
 };
 
 // ----------------------------------------------------------------------
 
 const WorkCard = (props: Props) => {
-  const { project, index } = props;
+  const { project } = props;
   const whileInView = useBreakpointValue(
     {
-      base: "showButton",
+      base: "inview",
       md: undefined,
     },
     { ssr: true }
   );
-
-  const [showDetails, detailsOptions] = useBoolean();
-
-  const colorScheme = useColorModeValue("whiteAlpha", "black");
+  const colorScheme = useColorModeValue("white", "gray.700");
 
   return (
     <MotionBox
-      initial={"hideButton"}
-      whileHover={"showButton"}
+      as={Card}
+      initial={"hide"}
+      whileHover={"show"}
       whileInView={whileInView}
-      sx={{
-        position: "relative",
-        aspectRatio: 9 / 12,
-        userSelect: "none",
-        borderRadius: 10,
-        overflow: "hidden",
-      }}
+      sx={{ overflow: "hidden", position: "relative" }}
     >
-      <Image
-        src={project.thumbnail.url}
-        alt={project.title}
-        fill
-        style={{ objectFit: "cover", userSelect: "none" }}
-      />
-
       <Box
-        position={"absolute"}
-        top={0}
-        left={0}
-        width={"100%"}
-        height={"100%"}
+        sx={{
+          aspectRatio: 16 / 9,
+          userSelect: "none",
+          position: "relative",
+        }}
       >
-        <MotionBox
-          as={Stack}
-          alignItems={"flex-start"}
-          backgroundColor={"primary.600"}
-          width={"100%"}
-          height={"100%"}
-          borderRadius={10}
-          p={4}
-          variants={detailsVariants}
-          transition={{
-            duration: 0.8,
-            type: "spring",
-            bounce: 0.4,
-          }}
-          animate={showDetails ? "show" : "hide"}
-        >
+        <Image
+          src={project.thumbnail.url}
+          alt={project.title}
+          fill
+          style={{ objectFit: "cover", userSelect: "none" }}
+        />
+
+        {/* {Array.isArray(project.technologies) && (
           <Flex
-            mb={"auto"}
+            position={"absolute"}
+            bottom={0}
+            left={0}
             width={"100%"}
-            alignItems={"center"}
-            justifyContent={"flex-end"}
-            gap={2}
+            flexWrap={"wrap"}
+            rowGap={1}
+            columnGap={1}
+            p={5}
           >
-            {/* {project?.slug && (
+            {project.technologies.map((tech, index) => (
+              <MotionBox
+                variants={tagsVariants}
+                transition={{
+                  type: "spring",
+                  bounce: 0.4,
+                  duration: 0.8,
+                  delay: index * 0.03,
+                }}
+                key={tech}
+              >
+                <Text
+                  fontSize={"xs"}
+                  textTransform={"uppercase"}
+                  fontWeight={"600"}
+                  py={0.5}
+                  bgColor={colorScheme}
+                  px={1.5}
+                  rounded={"sm"}
+                >
+                  {tech}
+                </Text>
+              </MotionBox>
+            ))}
+          </Flex>
+        )} */}
+      </Box>
+      <CardBody>
+        <Text fontSize={"xl"} fontWeight={"bold"} mb={1}>
+          {project.title}
+        </Text>
+        <Text fontSize={"md"} opacity={0.8} fontWeight={"medium"}>
+          {project.description}
+        </Text>
+      </CardBody>
+
+      <Flex
+        width={"100%"}
+        alignItems={"center"}
+        justifyContent={"flex-end"}
+        gap={2}
+        position={"absolute"}
+        top={5}
+        right={5}
+      >
+        {/* {project?.slug && (
               <IconButton
                 aria-label="Case Study"
                 title="Case Study"
@@ -123,54 +150,24 @@ const WorkCard = (props: Props) => {
                 variant={"solid"}
               />
             )} */}
-            {project.reference && (
-              <IconButton
-                aria-label="Reference"
-                title="Preview"
-                icon={<FiExternalLink />}
-                as={Link}
-                href={project.reference}
-                target="_blank"
-                variant={"solid"}
-              />
-            )}
-          </Flex>
-          <Text fontSize={"xl"} color={"white"} fontWeight={"bold"}>
-            {project.title}
-          </Text>
-          <Text fontSize={"md"} color={"gray.100"} fontWeight={"medium"} mb={5}>
-            {project.description}
-          </Text>
 
-          <Flex flexWrap={"wrap"} gap={1}>
-            {project.technologies.map((tech) => (
-              <Badge
-                variant={"solid"}
-                colorScheme={colorScheme}
-                px={2}
-                key={tech}
-              >
-                {tech}
-              </Badge>
-            ))}
-          </Flex>
-        </MotionBox>
-      </Box>
-
-      <Box position={"absolute"} top={5} left={5}>
-        <MotionBox
-          variants={buttonVariants}
-          transition={{ type: "spring", bounce: 0.4, duration: 0.8 }}
-        >
-          <IconButton
-            aria-label="Work Details"
-            icon={showDetails ? <FaBookOpen /> : <FaBook />}
-            onClick={detailsOptions.toggle}
-            variant={"solid"}
-            colorScheme="primary"
-          />
-        </MotionBox>
-      </Box>
+        {project.reference && (
+          <MotionBox
+            variants={buttonVariants}
+            transition={{ type: "spring", bounce: 0.4, duration: 0.8 }}
+          >
+            <IconButton
+              aria-label="Reference"
+              title="Preview"
+              icon={<FiExternalLink />}
+              as={Link}
+              href={project.reference}
+              target="_blank"
+              variant={"solid"}
+            />
+          </MotionBox>
+        )}
+      </Flex>
     </MotionBox>
   );
 };
